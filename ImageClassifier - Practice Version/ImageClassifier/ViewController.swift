c23bd2bfa7e9fa8f9347674ef95993b39fb9b6f7
sanad <mohdsanadzakirizvi@gmail.com>
2019-11-10 01:44:56 +0530

    added practice and complete versions

---
+++ ImageClassifier - Practice Version/ImageClassifier/ViewController.swift
@@ -0,0 +1,99 @@
+import CoreML
+import Vision
+import UIKit
+
+class ViewController: UIViewController {
+
+  // MARK: - IBOutlets
+  @IBOutlet weak var scene: UIImageView!
+  @IBOutlet weak var answerLabel: UILabel!
+
+  // MARK: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    guard let image = UIImage(named: "scenery") else {
+      fatalError("no starting image")
+    }
+
+    scene.image = image
+    
+    guard let ciImage = CIImage(image: image) else {
+      fatalError("couldn't convert UIImage to CIImage")
+    }
+
+    detectScene(image: ciImage)
+  }
+}
+
+// MARK: - IBActions
+extension ViewController {
+
+  @IBAction func pickImage(_ sender: Any) {
+    let pickerController = UIImagePickerController()
+    pickerController.delegate = self
+    pickerController.sourceType = .savedPhotosAlbum
+    present(pickerController, animated: true)
+  }
+}
+
+// MARK: - Methods
+extension ViewController {
+
+  func detectScene(image: CIImage) {
+    answerLabel.text = "detecting..."
+  
+    // Load the ML model through its generated class
+    guard let model = try? VNCoreMLModel(for: Resnet50().model) else {
+      fatalError("can't load Places ML model")
+    }
+    
+    // Create a Vision request with completion handler
+    let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+      let results = request.results as? [VNClassificationObservation]
+
+      var outputText = ""
+      
+      for res in results!{
+        outputText += "\(Int(res.confidence * 100))% it's \(res.identifier)\n"
+      }
+      DispatchQueue.main.async { [weak self] in
+        self?.answerLabel.text! = outputText
+      }
+    }
+    
+    // Run the CoreML3 Resnet50 classifier on global dispatch queue
+    let handler = VNImageRequestHandler(ciImage: image)
+    DispatchQueue.global(qos: .userInteractive).async {
+      do {
+        try handler.perform([request])
+      } catch {
+        print(error)
+      }
+    }
+
+  }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate {
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    dismiss(animated: true)
+
+    guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+      fatalError("couldn't load image from Photos")
+    }
+
+    scene.image = image
+    guard let ciImage = CIImage(image: image) else {
+      fatalError("couldn't convert UIImage to CIImage")
+    }
+
+    detectScene(image: ciImage)
+  }
+}
+
+// MARK: - UINavigationControllerDelegate
+extension ViewController: UINavigationControllerDelegate {
+}
